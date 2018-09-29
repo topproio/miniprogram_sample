46ea2e5d6d56478bc6d0e0c5d544d12c0ab8af72
hantts666 <123456>
2018-09-29 15:30:00 +0800

    another changes

--- src/pages/animate/animate.js
+++ src/pages/animate/animate.js
@@ -7,10 +7,10 @@ Page({
         animations: ['fadeIn', 'fadeOut', 'bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble'],
         currClass:'fadeIn animated box',
         animationData: {},
-        animation: null,
-        animateStract:[],
-        loopFlag:true
+        animateQueue:[],
     },
+    loopFlag: true,
+    animation: null,
     onPickerChange: function(v){
         let pickClssName = this.data.animations[v.detail.value];
         this.setData({
@@ -20,42 +20,42 @@ Page({
     onTap: function(v){
         let t = v.detail.y - v.currentTarget.offsetTop;
         let l = v.detail.x;
-        let ani = this.data.animation;
-        let stract = this.data.animateStract;
+        let ani = this.animation;
+        let stract = this.data.animateQueue;
         ani.top(t).left(l).rotateZ(getRandomAng()).step();
-        function getRandomAng(){
-            return Math.floor(Math.random()*4)*360;
-        }
-        this.data.animateStract.push({
+        this.data.animateQueue.push({
             ani:ani.export(),
             t:t,
             l:l,
             i:stract.length === 0?1:(stract[stract.length-1].i + 1)
         });
         this.setData({
-            animateStract:this.data.animateStract
+            animateQueue:this.data.animateQueue
         });
         this.stractChangeHandler();
+        function getRandomAng(){
+            return Math.floor(Math.random()*4)*360;
+        }
     },
     stractChangeHandler: function(){
-        if(!this.data.loopFlag){
+        if(!this.loopFlag){
             return;
         }
-        this.data.loopFlag = false;
+        this.loopFlag = false;
         let _this = this;
         loopStract();
         function loopStract(){
-            if(_this.data.animateStract.length === 0){
-                _this.data.loopFlag = true;
+            if(_this.data.animateQueue.length === 0){
+                _this.loopFlag = true;
                 return;
             }else{
                 _this.setData({
-                    animationData: _this.data.animateStract[0].ani
+                    animationData: _this.data.animateQueue[0].ani
                 });
                 setTimeout(function() {
-                    _this.data.animateStract.shift();
+                    _this.data.animateQueue.shift();
                     _this.setData({
-                        animateStract: _this.data.animateStract,
+                        animateQueue: _this.data.animateQueue,
                     });
                     loopStract();
                 }, 1000);
@@ -68,9 +68,7 @@ Page({
             timingFunction: 'ease'
         });
         ani.top(0).left(0).step();
-        this.setData({
-            animation: ani
-        });
+        this.animation = ani;
         this.setData({
             animationData: ani.export()
         });
