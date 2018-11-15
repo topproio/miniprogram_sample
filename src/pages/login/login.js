d351185b25a9a73d7fc152ff6b07d6ee890a30d3
anngirl <13718391141@163.com>
2018-11-15 17:27:43 +0800

    封装公共非空验证

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,12 @@
-const app=getApp();
-
+const app = getApp();
+var isEmpty_g = 0;
 Page({
     data: {
         items: ['爱好1', '爱好2', '爱好3'],
         hoblestr: '',
     },
     formSubmit(event) {
+        isEmpty_g = 0;
         let userName=event.detail.value.userName;
         let passWord=event.detail.value.passWord;
         let sex=event.detail.value.sex;
@@ -19,24 +20,43 @@ Page({
                 hoblestr: '',
             });
         }
-        if(userName===''){
-            app.msg('用户名不为空');
-            return;
-        }
-        if(passWord===''){
-            app.msg('密码不为空');
-            return;
-        }
-        if(sex===''){
-            app.msg('请选择性别');
-            return;
-        }
-        let datas={
-            hoblestr: this.data.hoblestr,
-            userName: userName,
-            passWord: passWord,
-            sex: sex,
-        };
-        console.error(datas);
-    }
+        let emptyArr = [{
+            name : `${userName}`,
+            text : '请输入用户名',
+            isEmpty : true
+        }, {
+            name : `${passWord}`,
+            text : '请输入密码',
+            isEmpty : true
+        }, {
+            name : `${sex}`,
+            text : '请选择性别',
+            isEmpty : true
+        }];
+        this.empty(emptyArr);
+        emptyArr.forEach(element => {
+            if(element.isEmpty){
+                isEmpty_g = 1;
+            }
+        });
+        if(isEmpty_g === 0){
+            let datas={
+                hoblestr: this.data.hoblestr,
+                userName: userName,
+                passWord: passWord,
+                sex: sex,
+            };
+            console.error('提交成功', datas);
+            app.msg('提交成功');
+        }        
+    },
+    empty(emptyArr) {
+        emptyArr.reverse().forEach(element => {
+            if(element.name === ''){
+                app.msg(element.text);
+            }else{
+                element.isEmpty = false;
+            }
+        });
+    },
 });
