228a2d627716fc467907577c088cede493bed934
hantts666 <976911704@qq.com>
2018-12-14 20:10:15 +0800

    add a cache version，don not merge！
    add a cache version，for checking code，don not merge！i wll fix it sunday

--- gulpfile.js
+++ gulpfile.js
@@ -64,3 +64,95 @@ gulp.task('watch', () => {
     gulp.watch('src/**', ['build:lint', 'build:style', 'build:page']);
 });
 
+// 以下为一些测试
+gulp.task('test', ['test:save2',]);
+
+gulp.task('test:save2',() => {
+    gulp.src(['dist/**/*.wxml','dist/**/*.wxss'])
+        .pipe(through.obj(function(file, enc, callback){
+            this.push(file.path);
+            callback()
+        }))
+        .on('data',function(xmlPath){
+            console.log(xmlPath)
+            xmlPath.replace('\\','/')
+            console.log(path.resolve('../../img/2.jpg'))
+            gulp.src(['dist/pages/test.wxml'])
+                .pipe(replace(/[\.|\.\.\/]+[(a-z0-9A-Z_)+/]+(\.jpg|\.png)/g, function(imgPath){
+                    //this.push(match)
+                    //getImgPath(imgPath,xmlPath)
+                    //console.log(imgPath,xmlPath)
+                    //return match.replace(/^[(\.)|(\.\.)\/]+/,uploadOption.viewPath)
+
+                    return imgPath;
+                }))
+        })
+})
+
+
+function getImgPath(imgPath,xmlPath){
+    console.log('xml',xmlPath)
+    console.log('img',imgPath)
+    var imgPathArr = imgPath.split("/")
+    var xmlPathArr = xmlPath.split("\\")
+    var dirPathArr = __dirname.split("\\")
+    xmlPathArr = xmlPathArr.slice(xmlPathArr.indexOf(dirPathArr.pop())+1,xmlPathArr.length)
+    // xmlPathArr.indexOf(dirPathArr.pop())
+    xmlPathArr.pop()
+    var abslutePath = dgOfChangePath(imgPathArr)
+    console.log('over',xmlPathArr)
+    console.log('___________________________________')
+    console.log('___________________________________')
+    function dgOfChangePath(imgPathArr){
+        if(imgPathArr.length == 0)return
+        var curr = imgPathArr.shift()
+        if(curr === ".."){
+            xmlPathArr.pop()
+            dgOfChangePath(imgPathArr)
+        }else if(curr === "."){
+            dgOfChangePath(imgPathArr)
+        } else if(/[a-zA-Z0-9_]+(.png|.jpg|.jpeg)$/.test(curr)){
+            xmlPathArr.push(curr)
+        }else {
+            //xmlPathArr.pop()
+            xmlPathArr.push(curr)
+            dgOfChangePath(imgPathArr)
+        }
+
+    }
+    return
+}
+function getRootPath(dir,path){
+
+}
+function GetAbsoluteUrl(oldUrl, relativeUrl, domain)
+{
+    var result = "";
+    var virtualPath = oldUrl;
+    if (!oldUrl.ToLower().Contains("http://")) //oldUrl不以http://开头，无法比较
+    {
+        return relativeUrl;
+    }
+    var fromDomain = domain;
+    if (fromDomain.LastIndexOf("/") == fromDomain.Length - 1)
+    {
+        fromDomain = fromDomain.Substring(0, fromDomain.Length - 1);
+    }
+    if (relativeUrl.IndexOf("/") == 0)
+    {
+        result = domain + relativeUrl;
+    }
+    else
+    {
+        var oldUrl1 = oldUrl.Substring(7);
+        if (oldUrl1.IndexOf("/") != -1)
+            oldUrl1 = oldUrl1.Substring(0, oldUrl1.LastIndexOf("/"));
+        while (relativeUrl.IndexOf("../") != -1)
+        {
+            relativeUrl = relativeUrl.Replace("../", "");
+            oldUrl1 = oldUrl1.Substring(0, oldUrl1.LastIndexOf("/"));
+        }
+        result = preHttp + oldUrl1 + "/" + relativeUrl;
+    }
+    return result;
+}
