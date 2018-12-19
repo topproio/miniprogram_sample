b18968e7067073c08444325d234402156f8254ff
daijinru <jeocat@163.com>
2018-12-19 19:39:49 +0800

    refactor: gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -3,62 +3,75 @@ const less = require('gulp-less');
 const rename = require('gulp-rename');
 const cssnano = require('gulp-cssnano');
 const eslint = require('gulp-eslint');
+const gulpLoadPlugins = require('gulp-load-plugins');
 
-gulp.task('build:lint', () => {
-    gulp.src('src/**/*.js')
-        .pipe(eslint({
-            configFile: './.eslintrc.js'
-        }))
-        .pipe(eslint.format());
+const gulpTaskList = require('fs').readdirSync('./gulp/tasks/');
+const config = require('./gulp/config.json');
+
+gulpLoadPlugins.eslint = eslint;
+gulpLoadPlugins.less = less;
+gulpLoadPlugins.rename = rename;
+gulpLoadPlugins.cssnano = cssnano;
+
+gulpTaskList.forEach(function (taskFile) {
+    require('./gulp/tasks/' + taskFile)(gulp, gulpLoadPlugins, config);
 });
 
-gulp.task('build:style', () => {
-    gulp.src(['src/pages/**/*.less',
-        'src/components/**/*.less',
-        'src/app.less'], { base: 'src' })
-        .pipe(less())
-        .pipe(cssnano({
-            zindex: false,
-            autoprefixer: false,
-            discardComments: { removeAll: true }
-        }))
-        .pipe(rename((path) => {
-            path.extname = '.wxss';
-        }))
-        .pipe(gulp.dest('dist'));
-});
+// gulp.task('build:lint', () => {
+//     gulp.src('src/**/*.js')
+//         .pipe(eslint({
+//             configFile: './.eslintrc.js'
+//         }))
+//         .pipe(eslint.format());
+// });
 
-gulp.task('build:page', () => {
-    gulp.src(['src/**/*',
-        '!src/**/*.less'], { base: 'src' })
-        .pipe(rename((path) => {
-            if (path.extname === '.html') {
-                path.extname = '.wxml';
-            }
-        }))
-        .pipe(gulp.dest('dist'));
-});
+// gulp.task('build:style', () => {
+//     gulp.src(['src/pages/**/*.less',
+//         'src/components/**/*.less',
+//         'src/app.less'], {base: 'src'})
+//         .pipe(less())
+//         .pipe(cssnano({
+//             zindex: false,
+//             autoprefixer: false,
+//             discardComments: {removeAll: true}
+//         }))
+//         .pipe(rename((path) => {
+//             path.extname = '.wxss';
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
+
+// gulp.task('build:page', () => {
+//     gulp.src(['src/**/*',
+//         '!src/**/*.less'], {base: 'src'})
+//         .pipe(rename((path) => {
+//             if (path.extname === '.html') {
+//                 path.extname = '.wxml';
+//             }
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
 
 
-const uploadOption = require('./uploadOption.js');
-    upload = require('gulp-qndn').upload,//七牛上传
-    replace = require('gulp-replace');
+// const uploadOption = require('./uploadOption.js');
+// const {upload} = require('gulp-qndn');
+// const replace = require('gulp-replace');
 
-gulp.task('build:uploadimage', () => {
-    gulp.src(['src/**/*.jpg','src/**/*.png'],{ base: 'src' })
-        .pipe(upload({qn: uploadOption.qnOption}));
-})
+// gulp.task('build:uploadimage', () => {
+//     gulp.src(['src/**/*.jpg', 'src/**/*.png'], {base: 'src'})
+//         .pipe(upload({qn: uploadOption.qnOption}));
+// });
 
-gulp.task('build:replace', () => {
-    gulp.src(['dist/**/*.wxml','dist/**/*.wxss'])
-        .pipe(replace(/[\.|\.\.\/]+[(a-z0-9A-Z_)+/]+(\.jpg|\.png)/g, function(match){
-            return match.replace(/^[(\.)|(\.\.)\/]+/,uploadOption.viewPath);
-        }))
-        .pipe(gulp.dest('dist'));
-})
+// gulp.task('build:replace', () => {
+//     gulp.src(['dist/**/*.wxml', 'dist/**/*.wxss'])
+//         .pipe(replace(/[\.|\.\.\/]+[(a-z0-9A-Z_)+/]+(\.jpg|\.png)/g, function (match) {
+//             return match.replace(/^[(\.)|(\.\.)\/]+/, uploadOption.viewPath);
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
 
 gulp.task('default', ['watch', 'build:lint', 'build:style', 'build:page']);
-gulp.task('uploadImg', ['build:uploadimage', 'build:replace']);
+// gulp.task('uploadImg', ['build:uploadimage', 'build:replace']);
 
 gulp.task('watch', () => {
     gulp.watch('src/**', ['build:lint', 'build:style', 'build:page']);
--- uploadOption.js
+++ uploadOption.js
@@ -1,9 +1,8 @@
-const accessKey = 'Z5eVKI63cMYXboRx19rI2SmPQiQWD0i3kFMlC6GR',// 填写七牛网的accessKey
-    secretKey = 'ry4Si2FO58PdbcZkr2coxm0xERHyV6XN7vULEqUu',// 填写七牛网的secretKey
-    bucket = 'hello',// 填写七牛网文件所在的镜像
-    domin = 'http://upload.qiniup.com',//上传地址
-    viewPath = "http://pjnxk0pmy.bkt.clouddn.com/"// 图片上传后的静态引用路径前缀
-
+const accessKey = 'Z5eVKI63cMYXboRx19rI2SmPQiQWD0i3kFMlC6GR'; // 填写七牛网的accessKey
+const secretKey = 'ry4Si2FO58PdbcZkr2coxm0xERHyV6XN7vULEqUu'; // 填写七牛网的secretKey
+const bucket = 'hello'; // 填写七牛网文件所在的镜像
+const domin = 'http://upload.qiniup.com'; // 上传地址
+const viewPath = 'http://pjnxk0pmy.bkt.clouddn.com/'; // 图片上传后的静态引用路径前缀
 
 const options = {
     accessKey: accessKey,
@@ -11,10 +10,9 @@ const options = {
     bucket: bucket,
     domin: domin,
     delete: false// （每次上传，如果你要删除同名文件就改为true）
-}
-//module.export.qnOption = $option
-//module.export.viewPath = $viewPath
+};
+
 module.exports = {
     qnOption: options,
     viewPath: viewPath
-}
+};
